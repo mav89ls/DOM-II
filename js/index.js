54b76304755e46c7295a7a89bc5bac4356fdaa49
Marco Vargas <mav89ls@gmail.com>
2019-05-04 11:20:49 -0700

    Started on a few events, added a drag event and some nav link events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+
+
+
+
+
+
+
+
+
+// Hover over the Fun Bus logo to double its size.
+const navLogoHeading = document.querySelector('.logo-heading');
+navLogoHeading.addEventListener("mouseover", (e) => e.target.style.transform = "scale(2.0)");
+navLogoHeading.addEventListener('mouseleave', (e) => e.target.style.transform = "scale(1.0)");
+
+// Drag the intro img to increase its size. 
+const funBusBanner = document.querySelector(".intro > img");
+funBusBanner.addEventListener('drag', (e) => e.target.style.transform = 'scale(1.25)');
+
+// Double click the nav links to change their background color to red.
+const nav = document.querySelector(".nav");
+nav.addEventListener("dblclick", (e) => { e.target.style.backgroundColor = 'red'; });
+
+// Press any keyboard key to make the header disappear. 
+const body = document.querySelector('body');
+const header = document.querySelector('.main-navigation');
+body.addEventListener("keydown", () => { header.style.opacity = "0" });
