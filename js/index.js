d5e7a2f2fe462843819ad2c1d41206eb0495a041
Marco Vargas <mav89ls@gmail.com>
2019-05-04 11:29:39 -0700

    added a fun alert pop up when the page refreshes

--- js/index.js
+++ js/index.js
@@ -19,7 +19,23 @@ const adventureAwaits = document.querySelectorAll('.text-content h2')
     //adventureAwaits.addEventListener('mousemove', (e) => {e.stopPropagation()})
 adventureAwaits.forEach(node => { node.addEventListener('mousemove', (e) => { e.target.style.color = 'Maroon' }) });
 
+window.addEventListener("load", function(event) {
+    alert("Hover and Click Away! There are 10 different things you can do on this site. Hover over the Fun Bus logo to change its size, drag the intro img to increase its size, double click the nav links to change their background color, press any keyboard key to make the header disappear, hover over the header to make the header reappear, hover over an expedition title to change its color to Green, hover over an Adventure Awaits title to change its color to Maroon, Double click a sign up button to prompt an input box for your name and then pop up an alert, Right click the footer to change its background color.");
+});
 
+
+// Double click a sign up button to prompt an input box and then an alert. 
+const button = document.querySelectorAll('.btn');
+button.forEach(node => {
+    node.addEventListener('dblclick', function() {
+        let name = prompt('Enter name');
+        alert(`${name} you are signed up!`);
+    })
+});
+
+// Right click the footer to change its background color to MediumTurquoise.
+footer = document.querySelector('.footer');
+footer.addEventListener('contextmenu', (e) => { e.target.style.backgroundColor = 'MediumTurquoise'; })
 
 // Hover over the Fun Bus logo to double its size.
 const navLogoHeading = document.querySelector('.logo-heading');
